828f13772dccf645c064d0b21ba645a82dca94b9
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-05-07 03:10:21 +0700

    Experimen dengan cara untuk read file

--- soal1/Server/server.c
+++ soal1/Server/server.c
@@ -13,6 +13,8 @@
 #define MAX_CONN 50
 #define MAX_CREDENTIALS_LENGTH 100
 #define MAX_INFORMATION_LENGTH 200
+#define EMPAT_KB 4096
+#define MAX_FILE_CHUNK EMPAT_KB
 
 bool isFileExists(char filename[]) {
     return access(filename, F_OK) == 0;
@@ -52,7 +54,7 @@ bool acceptConnection(int * new_socket, int * server_fd, struct sockaddr_in * ad
     return (*new_socket = accept(*server_fd, (struct sockaddr *)address, (socklen_t*)&addrlen)) != -1;
 }
 
-bool login(int socket) {
+bool login(int socket, char authenticatedUser[]) {
     char credentials[MAX_CREDENTIALS_LENGTH], file_credentials[MAX_CREDENTIALS_LENGTH];
     memset(credentials, 0, MAX_CREDENTIALS_LENGTH);
     int readStatus = read(socket, credentials, MAX_CREDENTIALS_LENGTH);
@@ -61,6 +63,7 @@ bool login(int socket) {
         if(strcmp(credentials, file_credentials) == 0) {
             printf("Akun %s telah login\n", strtok(credentials, ":"));
             send(socket, "true", sizeof("true"), 0);
+            strcpy(authenticatedUser, credentials);
             fclose(fileAkun);
             return true;
         }
@@ -86,7 +89,7 @@ bool daftar(int socket) {
     return false;
 }
 
-bool authentication(int socket) {
+bool authentication(int socket, char authenticatedUser[]) {
     char action[10];
     memset(action, 0, sizeof(action));
     int readStatus = read(socket, action, sizeof(action));
@@ -94,7 +97,7 @@ bool authentication(int socket) {
         daftar(socket);
         return false;
     } else if (strcmp(action, "login") == 0) {
-        return login(socket);
+        return login(socket, authenticatedUser);
     }
     return false;
 }
@@ -104,7 +107,21 @@ bool isFolderExists(char foldername[]) {
     return dir != NULL;
 }
 
-bool addDataBuku(int socket) {
+FILE * readandSavefile(int socket, char filename[]) {
+    char isEOF[10];
+    char chunk[MAX_FILE_CHUNK + 1];
+    FILE * file = fopen(filename, "w");
+    do {
+        memset(isEOF, 0, sizeof(isEOF));
+        read(socket, isEOF, sizeof(isEOF));
+        memset(chunk, 0, MAX_FILE_CHUNK + 1);
+        read(socket, chunk, sizeof(chunk));
+        fprintf(file, "%s", chunk);
+    } while (strcmp(isEOF, "true") != 0);
+    return file;
+}
+
+bool addDataBuku(int socket, char *filename) {
     char information[MAX_INFORMATION_LENGTH];
     memset(information, 0, MAX_INFORMATION_LENGTH);
     int readStatus = read(socket, information, MAX_INFORMATION_LENGTH);
@@ -112,25 +129,48 @@ bool addDataBuku(int socket) {
     FILE * fileFile = fopen("files.tsv", "a");
     if(fileFile && readStatus > 0 && fprintf(fileFile, "%s\n", information)) {
         fclose(fileFile);
+        filename = strtok(information, "|");
         return true;
     }
     fclose(fileFile);
     return false;
 }
 
-bool addBuku(int socket) {
-    if(!addDataBuku(socket)) {
+void logging(const char event[], const char filename[], char authenticatedUser[]) {
+    FILE * logFile = fopen("running.log", "a");
+    if(logFile) {
+        fprintf(logFile, "%s : %s (%s)\n", event, filename, authenticatedUser);
+        fclose(logFile);
+    }
+}
+
+void addBuku(int socket, char authenticatedUser[]) {
+    char *filename;
+    if(!addDataBuku(socket, filename)) {
         send(socket, "false", sizeof("false"), 0);
         return false;
     }
-    // sendfile
+    FILE * file = readandSavefile(socket, filename);
+    if(file == NULL) {
+        send(socket, "false", sizeof("false"), 0);
+    }
+    fclose(file);
+    send(socket, "true", sizeof("true"), 0);
+    logging("Tambah", filename, authenticatedUser);
 }
 
-void app(int arg) {
-    int socket = arg;
-    while(!authentication(socket));
+void app(int socket) {
+    char authenticatedUser[MAX_CREDENTIALS_LENGTH];
+    while(!authentication(socket, authenticatedUser));
     printf("User telah terautentikasi\n");
-
+    while(true) {
+        char action[MAX_INFORMATION_LENGTH];
+        memset(action, 0, MAX_INFORMATION_LENGTH);
+        int readStatus = read(socket, action, MAX_INFORMATION_LENGTH);
+        if(readStatus > 0 && strcmp("add", action) == 0) {
+            addBuku(socket, authenticatedUser);
+        }
+    }
 }
 
 int main() {
